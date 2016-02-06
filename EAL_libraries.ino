a79c8b8cd090a7b415d0b0d17da732eda82b940f
mloispro <olson_mitchell@hotmail.com>
2016-02-05 21:56:24 -0600

    r

--- EAL_libraries.ino
+++ EAL_libraries.ino
@@ -1,13 +1,25 @@
 
 
+//#include "FishFeeder\FishFeeder.h"
+
+
+
+//#include <FishFeeder.h>
+
 //#include <Motor.h>
-#include <Servo.h>
-#include <SerialExt.h>
+//#include <Motor.h>
+#include <Motor.h>
+#include "StandardCplusplus.h"
+#include <vector>
+#include "Servo.h"
+#include "SerialExt.h"
+
 using namespace Utils;
 
 void setup() {
 	// initialize digital pin 13 as an output.
 	pinMode(13, OUTPUT);
+	//FishFeeder::CreateFeeders(2, 7);
 }
 
 // the loop function runs over and over again forever
@@ -17,4 +29,8 @@ void loop() {
 	delay(1000);              // wait for a second
 	digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
 	delay(1000);              // wait for a second
+	//FishFeeder feeder(Servo(), 7, 2);
+	//FishFeeder::Feeders.push_back(feeder);
+
+
 }
